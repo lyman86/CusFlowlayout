962d0000f8edf44eb68528e88a61676995e87b78
luoyan <1050032593@qq.com>
2018-04-19 14:03:57 +0800

    图片的提交和修改代码

--- app/src/main/java/ly/mysummery03/MainActivity.java
+++ app/src/main/java/ly/mysummery03/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -72,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
         for (String s:list) {
             str +=s+"  ";
         }
-        Log.e(TAG,str);
+        Toast.makeText(this,"您选择的是："+str,Toast.LENGTH_SHORT).show();
         Log.e(TAG,baseCusFlowLayout.getAllHeight()+"  ----");
 //        List<InfoBean>list = baseCusFlowLayout.getSelect();
 //        Map<Integer,String>map = baseCusFlowLayout.getMapStr();
